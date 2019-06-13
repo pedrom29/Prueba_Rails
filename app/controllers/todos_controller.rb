ffe47a1393e64ea3177306b8d0efe1042e85d314
Pedro <pedromatu29@gmail.com>
2019-06-12 21:51:47 -0400

    add_show_edit_update

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,33 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @all = Todo.new(todo_params)
+    @all.save
+    redirect_to todos_path
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
